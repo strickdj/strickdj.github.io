f5b8a6ee4a8ec4a0efb5fd41cf03159191f1f249
Daris Strickland <daris.strickland@gmail.com>
2021-01-22 12:02:33 -0500

    more 404 styles

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,8 +6,10 @@ const NotFoundPage = ({ data, location }) => {
   return (
     <Layout location={location}>
       <Seo title="404: Not Found" />
-      <h1>404: Not Found</h1>
-      <p>The content you are looking for could not be found.</p>
+      <section className="p-5 xl:p-16 flex-1 text-center">
+        <h1 className="mb-10">404: Not Found</h1>
+        <p>The content you are looking for could not be found.</p>
+      </section>
     </Layout>
   )
 }
